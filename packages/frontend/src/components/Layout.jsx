5961aadcc18e522b6e9a792ac1816a12fcacee66
Anselme SDR <a.schneider@vold.lu>
2026-01-23 21:34:49 +0100

    fix: footer sticky at bottom

--- packages/frontend/src/components/Layout.jsx
+++ packages/frontend/src/components/Layout.jsx
@@ -195,7 +195,7 @@ export default function Layout() {
       </aside>
 
       {/* Main content */}
-      <main className="flex-1 overflow-auto flex flex-col">
+      <main className="flex-1 flex flex-col min-h-0">
         <div className="flex-1 overflow-auto">
           <Outlet />
         </div>
